e5e197d869dee64065f52e8b9a80fccaa2fa25d4
Arkhom Suwanprasert <arkhoms@gmail.com>
2019-09-17 11:03:24 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('arkhom', function () {
+    return "อาคม";
+});
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
